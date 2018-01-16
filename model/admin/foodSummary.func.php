fe2e8fe8f161b90d5b0d9df94509a4ad3580bae1
mengyuhan <simonhmy@gmail.com>
2018-01-16 07:32:27 -0800

    add pagination

--- model/admin/foodSummary.func.php
+++ model/admin/foodSummary.func.php
@@ -1,13 +1,20 @@
+
 <?php if(!defined('In_System')) exit("Access Denied");
 
+include_once "paginator.php";
 
 Class FoodSummary{
 
     public function foodSummaryListing(){
         global $mysqli;
+        $limit      = ( isset( $_GET['limit'] ) ) ? $_GET['limit'] : 25;
+        $page       = ( isset( $_GET['page'] ) ) ? $_GET['page'] : 1;
+        $links      = ( isset( $_GET['links'] ) ) ? $_GET['links'] : 7;
         $query="SELECT users.uid,users.username,users.phone,users.address,food_service.time,food_service.dateToDeliver,food_service.service1,food_service.service2 FROM food_service INNER JOIN users ON users.salt=food_service.user";
-        $result = mysqli_fetch_all($mysqli->query($query), MYSQLI_ASSOC);
-		if(sizeof($result) > 0){
+        $Paginator  = new Paginator( $mysqli, $query );
+        $result    = $Paginator->getData( $limit, $page);
+        //$result = mysqli_fetch_all($mysqli->query($query), MYSQLI_ASSOC);
+		if(sizeof($result->data) > 0){
             echo "
             <table class=\"table table-hover table-info\">
                 <tr>
@@ -19,7 +26,7 @@ Class FoodSummary{
                     <th>种类</th> 
                 </tr>
             ";
-            foreach($result as $Summary){
+            foreach($result->data as $Summary){
                 $type = ($Summary['service1']==1?"月子餐":"").' '.($Summary['service2']==1?"待产餐":"");
                 echo sprintf("
                     <tr>
@@ -48,6 +55,10 @@ Class FoodSummary{
                     </tr>
                 </table>
             ";
-		}
+        }
+        echo $Paginator->createLinks( $links, 'pagination' );
     }
 }
+
+
+
