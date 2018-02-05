11e37f98ae8b21d6faf1e546a50bfa4b69b751cb
mengyuhan <simonhmy@gmail.com>
2018-02-05 07:29:14 -0800

    complete admin purchase_v2

--- admin/model/admin.purchase.php
+++ admin/model/admin.purchase.php
@@ -2,44 +2,54 @@
 Class Purchase{
     public $time = "";
     public $date;
-    public $itemList = ["doujiang","tiandoujiang"];
+    public $itemList = ["doujiang","tiandoujiang","niunai","guozhi","furu","zhacai","laoganma","ganlancai","xianyadan","huashengjiang","caomeijiang","shengjidan","maipian","culiangmianbao","doushabao","xiaomantou","shouzhuabing","jiaozi","miantiao","dami","xiaomi","hongdou","lvdou","pingguo","xiangjiao","chengzi","guoli","juzi","xihongshi","bocai","digua","huanggua","tudou","you","yan","jiang","cu","tang"];
+    //
+    public $itemChineseList = ["豆浆","甜豆浆","牛奶","果汁","腐乳","榨菜","老干妈","橄榄菜","咸鸭蛋","花生酱","草莓酱","生鸡蛋","麦片","粗粮面包","豆沙包","小馒头","手抓饼","饺子","面条","大米","小米","红豆","绿豆","苹果","香蕉","橙子","果梨","橘子","西红柿","菠菜","地瓜","黄瓜","土豆","油","盐","酱","醋","糖"];
     public $table = "purchase_service INNER JOIN users ON users.salt=purchase_service.user";
     public $rawStates = "users.uid,users.username,users.phone,purchase_service.date,purchase_service.property,purchase_service.origin_address";
     public $completeStates = "";
     public function generateState() {
         $size = sizeof($this->itemList);
-        $this->completeStates . $this->rawStates;
+        $this->completeStates = $this->completeStates . $this->rawStates;
         while ($size>0){
-            $this->completeStates . ",purchase_service." . $this->itemList[$size];
+            $this->completeStates = $this->completeStates . ",purchase_service." . $this->itemList[$size-1];
             $size--;
         }
     }
     public function totalQuery($condition) {
-        return "select sum(potato), sum(tomato), sum(rice) from ".$this->table. " where ".$condition;
+        $size = sizeof($this->itemList);
+        $totalString = "select sum(doujiang)";
+        $i = 1;
+        while ($i<$size){
+            $totalString = $totalString . ",sum(" . $this->itemList[$i] . ")";
+            $i++;
+        }
+        return $totalString . " from ".$this->table. " where ".$condition;
     }
     public function thisWeekListing(){
         $condition = "(SUBDATE(CURDATE(),DATE_FORMAT(CURDATE(),'%w')+2))<=DATE(purchase_service.date) and DATE(purchase_service.date)<=(SUBDATE(CURDATE(),DATE_FORMAT(CURDATE(),'%w')-4))";
-        $query="SELECT ". $this->stats." from ".$this->table. " where ".$condition;
+        $query="SELECT ". $this->completeStates." from ".$this->table. " where ".$condition;
         $this->time = "上周五到这周四";    
         $this->showResult($query, $this -> totalQuery($condition));
     }
 
     public function nextWeekListing(){
         $condition = "(SUBDATE(CURDATE(),DATE_FORMAT(CURDATE(),'%w')-5))<=DATE(purchase_service.date) and DATE(purchase_service.date)<=(SUBDATE(CURDATE(),DATE_FORMAT(CURDATE(),'%w')-11))";
-        $query="SELECT ". $this->stats." FROM ".$this->table. " WHERE ".$condition;
+        $query="SELECT ". $this->completeStates." FROM ".$this->table. " WHERE ".$condition;
         $this->time = "这周五到下周四";
         $this->showResult($query, $this -> totalQuery($condition));
     }
 
     public function twoWeekLater(){
         $condition = "(SUBDATE(CURDATE(),DATE_FORMAT(CURDATE(),'%w')-12))<=DATE(purchase_service.date) and DATE(purchase_service.date)<=(SUBDATE(CURDATE(),DATE_FORMAT(CURDATE(),'%w')-18))";
-        $query="SELECT ". $this->stats." FROM ".$this->table. " WHERE ".$condition;
+        $query="SELECT ". $this->completeStates." FROM ".$this->table. " WHERE ".$condition;
         $this->time = "下周五到下下周四";
         $this->showResult($query, $this -> totalQuery($condition));
     }
 
     public function showResult($query,$totalQuery) {
         $pageString="";
+        $size = sizeof($this->itemList);
         $tableString ="<br/>
         <div class=\"table-responsive\">
         <table class=\"table table-bordered\">
@@ -47,14 +57,13 @@ Class Purchase{
         <tr>
             <th>#</th>
             <th>客户名</th>
-            <th>电话</th>
-            <th>土豆</th>
-            <th>番茄</th>
-            <th>大米</th>
-        </tr>
-        </thead> 
-        <tbody> 
-        ";
+            <th>电话</th>";
+            for ($i = 0; $i<$size; $i++) {
+                $tableString = $tableString . "<th>" . $this->itemChineseList[$i] ."<th>";
+            }
+        $tableString= $tableString . "</tr>
+                        </thead> 
+                        <tbody>";
 
         global $mysqli;
         $limit      = ( isset( $_GET['limit'] ) ) ? $_GET['limit'] : 10;
@@ -63,22 +72,18 @@ Class Purchase{
         $Paginator  = new Paginator( $mysqli, $query);
         $result    = $Paginator->getData( $limit, $page);
         $rs = $mysqli->query( $totalQuery );
-        $row = $rs->fetch_assoc();
-        $totalPotato=$row['sum(potato)'];
-        $totalTomato=$row['sum(tomato)'];
-        $totalRice=$row['sum(rice)'];
+        $row2 = $rs->fetch_assoc();
 		if(sizeof($result->data) > 0){
             foreach($result->data as $Summary){
                 $row = "
                     <tr>
                         <td>".$Summary['uid']."</td>
                         <td>".$Summary['username']."</td>
-                        <td><a href=\"tel:".$Summary['phone']."\">".$Summary['phone']."</a></td> 
-                        <td>".$Summary['potato']."</td> 
-                        <td>".$Summary['tomato']."</td> 
-                        <td>".$Summary['rice']."</td> 
-                    </tr>
-            ";
+                        <td><a href=\"tel:".$Summary['phone']."\">".$Summary['phone']."</a></td>"; 
+                for ($i = 0; $i<$size; $i++) {
+                    $row = $row . "<th>" . $Summary[$this->itemList[$i]] ."<th>";
+                }
+                $row = $row . "</tr>";
             $tableString=$tableString.$row;
             }
             $tableString=$tableString.
@@ -90,7 +95,11 @@ Class Purchase{
                 printf("undefined pageType");
                 exit();
             }
-             echo sprintf( "<br/><h5>%s一共%d个土豆，%d个番茄，%d袋大米</h5>",$this->time, $totalPotato,$totalTomato,$totalRice);
+            $printString = "<br/><h5>" . $this->time ."一共";
+            for ($i = 0; $i<$size; $i++) {
+                $printString = $printString . $row2['sum(' . $this->itemList[$i] . ')'] . $this->itemChineseList[$i] . " ";
+            }
+             echo $printString;
              $pageString=$Paginator->createLinks($pageType,$links, 'pagination pagination-sm');
             }else{
             $tableString=$tableString."</table></div>";
